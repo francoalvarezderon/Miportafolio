f22235746efb16095f36fbea3510c34c1066e9fe
francoalvarezderon <francoalvarezderon53@gmail.com>
2025-08-15 19:36:14 -0300

    Camios en el html, css y js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-window.alert("¡Hola, que gusto verte por aquí!"); // Muestra un mensaje de alerta al cargar la página
- 
+window.alert("¡Hola, que gusto verte por aquí!"); // Muestra un mensaje de alerta al cargar
+//  la página
+
+/*function animacion () {
+    let textoAnimacion = [
+    ["F", "r", "a", "n", "c", "o"],
+    ["Á", "l", "v", "a", "r", "e", "z", "d", "e", "R", "o", "n"]
+    ];
+    let letraContador=-1 ;
+    let nivelArray = 0;
+
+    const contenedorAnimacion= document.querySelector(".miNombre");
+
+    function pintarTexto() {
+        letraContador++;
+        contenedorAnimacion.textContent += textoAnimacion[nivelArray][letraContador];
+
+        if (letraContador===textoAnimacion[nivelArray].length-1) {
+            clearInterval(intervalo);
+        }
+    }
+    let intervalo = setInterval(pintarTexto, 150);
+}
+window.addEventListener("load", animacion);
+*/
